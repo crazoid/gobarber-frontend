e029d53f68cf3f66b94122aa7d205c9133757e84
Fabio Palhano <crazoid@gmail.com>
2020-12-09 15:20:44 -0300

    styles

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { FiLogIn } from 'react-icons/fi';
+
+import { Container, Content, Background } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignIn: React.FC = () => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="GoBarber"/>
+      <form>
+        <h1>Faça seu login</h1>
+        <Input name="email" type="text" placeholder="E-mail" id=""/>
+        <Input name="password" type="text" placeholder="Senha" id=""/>
+        <Button type="submit">Entrar</Button>
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+      <a href="login">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
+);
+
+export default SignIn;
